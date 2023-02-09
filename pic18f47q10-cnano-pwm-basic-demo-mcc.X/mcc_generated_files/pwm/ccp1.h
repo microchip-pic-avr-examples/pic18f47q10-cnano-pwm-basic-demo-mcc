e4e755b3a7e77fc7bfb7f336db5b24c51b522581
Teodor-Emilian Petre <Teodor-Emilian.Petre@microchip.com>
2023-02-09 13:50:01 +0200

    MPAE-15434: Example updates

--- pic18f47q10-cnano-pwm-basic-demo-mcc.X/mcc_generated_files/pwm/ccp1.h
+++ pic18f47q10-cnano-pwm-basic-demo-mcc.X/mcc_generated_files/pwm/ccp1.h
@@ -1,56 +1,33 @@
 /**
-  PWM1 Generated Driver File
-
-  @Company
-    Microchip Technology Inc.
-
-  @File Name
-    pwm1.h
-
-  @Summary
-    This is the generated driver implementation file for the PWM1 driver using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
-  @Description
-    This header file provides implementations for driver APIs for PWM1.
-    Generation Information :
-        Driver Version    :  2.01
-    The generated drivers are tested against the following:
-        Compiler          :  XC8 v2.20 and above
-        MPLAB             :  MPLABX v5.40
+ * PWM1 Generated Driver API Header File.
+ * 
+ * @file ccp1.h
+ * 
+ * @defgroup pwm1 PWM1
+ * 
+ * @brief This file contains the API prototypes for the PWM1 module.
+ *
+ * @version PWM1 Driver Version 2.0.2
 */
-
 /*
-Copyright (c) [2012-2020] Microchip Technology Inc.  
+© [2023] Microchip Technology Inc. and its subsidiaries.
 
-    All rights reserved.
-
-    You are permitted to use the accompanying software and its derivatives 
-    with Microchip products. See the Microchip license agreement accompanying 
-    this software, if any, for additional info regarding your rights and 
-    obligations.
-    
-    MICROCHIP SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT 
-    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT 
-    LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT 
-    AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP OR ITS
-    LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT, NEGLIGENCE, STRICT 
-    LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER LEGAL EQUITABLE 
-    THEORY FOR ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES INCLUDING BUT NOT 
-    LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES, 
-    OR OTHER SIMILAR COSTS. 
-    
-    To the fullest extend allowed by law, Microchip and its licensors 
-    liability will not exceed the amount of fees, if any, that you paid 
-    directly to Microchip to use this software. 
-    
-    THIRD PARTY SOFTWARE:  Notwithstanding anything to the contrary, any 
-    third party software accompanying this software is subject to the terms 
-    and conditions of the third party's license agreement.  To the extent 
-    required by third party licenses covering such third party software, 
-    the terms of such license will apply in lieu of the terms provided in 
-    this notice or applicable license.  To the extent the terms of such 
-    third party licenses prohibit any of the restrictions described here, 
-    such restrictions will not apply to such third party software.
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
 */
 
 #ifndef PWM1_H
@@ -64,111 +41,32 @@ Copyright (c) [2012-2020] Microchip Technology Inc.
 #include <stdint.h>
 #include <stdbool.h>
 
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-    extern "C" {
-
-#endif
-
-/**
-  Section: PWM1 Module APIs
-*/
-
-/**
-  @Summary
-    Initializes the PWM1
-
-  @Description
-    This routine initializes the PWM1 module.
-    This routine must be called before any other PWM1 routine is called.
-    This routine should only be called once during system initialization.
-
-  @Preconditions
-    None
-
-  @Param
-    None
-
-  @Returns
-    None
-
-  @Comment
-    
-
- @Example
-    <code>
-    uint16_t dutycycle;
-
-    PWM1_Initialize();
-	  PWM1_LoadDutyValue(dutycycle);
-    </code>
+ /**
+ * @ingroup pwm1
+ * @brief Initializes the CCP1 module. This is called only once before calling other CCP1 APIs.
+ * @param None.
+ * @return None.
  */
 void PWM1_Initialize(void);
 
 /**
-  @Summary
-    Loads 16-bit duty cycle.
-
-  @Description
-    This routine loads the 16 bit duty cycle value.
-
-  @Preconditions
-    PWM1_Initialize() function should have been called
-    before calling this function.
-
-  @Param
-    Pass 16bit duty cycle value.
-
-  @Returns
-    None
-
-  @Example
-    <code>
-    uint16_t dutycycle;
-
-    PWM1_Initialize();
-    PWM1_LoadDutyValue(dutycycle);
-    </code>
-*/
+ * @ingroup pwm1
+ * @brief Loads the 16-bit duty cycle value.
+ * @pre PWM1_Initialize() is already called.
+ * @param dutyValue - 16-bit duty cycle value.
+ * @return None.
+ */
 void PWM1_LoadDutyValue(uint16_t dutyValue);
 
 /**
-  @Summary
-    Read pwm output status.
-
-  @Description
-    This routine returns the pwm output status.
-
-  @Preconditions
-    PWM1_Initialize() function should have been
-    called before calling this function.
-
-  @Param
-    None
-
-  @Returns
-    true : output high
-    false: output low
-
-  @Example
-    <code>
-    uint16_t dutyCycle;
-    bool status;
-    PWM1_Initialize();
-    PWM1_LoadDutyValue(dutyCycle);
-    while(1)
-    {
-            status = PWM1_OutputStatusGet();
-    }
-    </code>
-*/
+ * @ingroup pwm1
+ * @brief Returns the PWM output status.
+ * @pre PWM1_Initialize() is already called.
+ * @param None.
+ * @retval True - CCP1 PWM output is high
+ * @retval False - CCP1 PWM output is low
+ */
 bool PWM1_OutputStatusGet(void);
-
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-    }
-
-#endif
 
 #endif //PWM1_H
 /**
